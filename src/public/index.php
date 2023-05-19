88ee8eeb001483734fd9802b27d6f19e39cd92f8
IsSSLab <manpuku.belly+github@gmail.com>
2023-05-19 16:12:11 +0200

    Remove form.php. Use index.php instead.

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,16 @@
 <?php
-  echo "Hello World!";
+  include_once($_SERVER['DOCUMENT_ROOT'].'/../utils.php');
+
+  session_start();
+  if (isset($_POST) && isset($_POST["workerID"])) {
+    $_SESSION["workerID"] = $_POST["workerID"];
+    clearPost();
+  }
+
+  if (@$_SESSION["workerID"]) {
+    $wid = $_SESSION["workerID"];
+    echo "Hello, $wid!";
+  } else {
+    include '../views/start.php';
+  }
 ?>
