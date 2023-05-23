6c30ed45a894269cfdbee7df8cc8d59668d8052f
IsSSLab <manpuku.belly+github@gmail.com>
2023-05-23 14:53:03 +0200

    Add a sample of PDO.

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,38 @@
 <?php
-  echo "Hello World!";
+  include_once($_SERVER['DOCUMENT_ROOT'].'/../dbconfig.php');
+  include_once($_SERVER['DOCUMENT_ROOT'].'/../utils.php');
+
+  session_start();
+  if (isset($_POST)) {
+    if (filter_input(INPUT_POST, "workerID")) {
+      
+      $workerID = filter_input(INPUT_POST, "workerID");
+      
+      try {
+        // -- Create a new record for this new user.
+        $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+        $sql = 'INSERT INTO Participant (id, last_seen_page_index, last_access_at, first_access_at) VALUES (:workerID, 0, now(), now())';
+        $statement = $pdo->prepare($sql);
+        $statement->bindValue(':workerID', $workerID);
+        $statement->execute();
+        // -- We can write the above syntax with a more simple way.
+        //    The reason why I use prepare() and execute() is to prevent SQL injection attacks.
+
+        $_SESSION["serial"] = $pdo->lastInsertId('serial');
+
+        $pdo = null; // -- Close the connection. But, it will be closed automatically when the script ends.
+      } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+      }
+
+      clearPost();
+    }
+  }
+
+  if (@$_SESSION["serial"]) {
+    echo "<p>Serial: ".$_SESSION['serial']."</p>";
+  } else {
+    include '../views/start.php';
+  }
+  
 ?>
---
+++ src/views/start.php
@@ -0,0 +1,4 @@
+<form method="POST">
+  <input type="text" name="workerID" />
+  <input type="submit" />
+</form>
